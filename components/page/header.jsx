86f319a61238f2c38ba83253aa32be75bb7f84d4
Анураг <anurag.kumar25@s.amity.edu>
2025-02-04 00:19:50 +0530

    Update header.jsx

--- components/page/header.jsx
+++ components/page/header.jsx
@@ -1,42 +1,66 @@
-"use client"
+"use client";
+import { useState } from "react";
 import { ModeToggle } from "../ModeToggle";
 import Logo from "./logo";
 import { Button } from "../ui/button";
 import Search from "./search";
-import { ChevronDown, ChevronLeft, Share2 } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { Search as SearchIcon, ChevronLeft } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
 import Link from "next/link";
 
 export default function Header() {
     const path = usePathname();
-    console.log(path);
-    
+    const router = useRouter();
+    const [isSearchVisible, setIsSearchVisible] = useState(false);
+
+    const toggleSearch = () => {
+        setIsSearchVisible(!isSearchVisible);
+    };
+
+    // Check if the current page is the player page
+    const isPlayerPage = path.includes("/player");
+    const showBackButton = path !== "/";
+
     return (
         <header className="sticky top-0 z-50 bg-white dark:bg-black shadow-md grid gap-3 pt-5 px-5 pb-5 md:px-20 lg:px-32">
             <div className="flex items-center justify-between">
-                {path === "/" ? (
-                    <Button 
-                        size="icon" 
-                        onClick={() => { navigator.share({ url: window.location.href }) }} 
-                        variant="outline" 
-                        className="rounded-full lg:-ml-4"
-                    >
-                        <Share2 className="w-4 h-4" />
-                    </Button>
-                ) : (
-                    <Button 
-                        size="icon" 
-                        asChild 
-                        variant="outline" 
+                {showBackButton ? (
+                    <Button
+                        size="icon"
+                        onClick={() => router.back()}
+                        variant="outline"
                         className="rounded-full"
                     >
-                        <Link href="/"><ChevronLeft className="w-5 h-5" /></Link>
+                        <ChevronLeft className="w-5 h-5" />
                     </Button>
+                ) : (
+                    <ModeToggle />
                 )}
                 <Logo />
-                <ModeToggle />
+                {isPlayerPage ? (
+                    <Button
+                        size="icon"
+                        asChild
+                        variant="outline"
+                        className="rounded-full"
+                    >
+                        <Link href="/">
+                            <ChevronLeft className="w-5 h-5" />
+                        </Link>
+                    </Button>
+                ) : (
+                    <Button
+                        size="icon"
+                        onClick={toggleSearch}
+                        variant="outline"
+                        className="rounded-full"
+                    >
+                        <SearchIcon className="w-4 h-4" />
+                    </Button>
+                )}
             </div>
-            <Search />
+            {/* Conditionally render the Search component based on isSearchVisible */}
+            {!isPlayerPage && <Search className={isSearchVisible ? "" : "hidden"} />}
         </header>
     );
 }
