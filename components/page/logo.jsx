f110931eed1b49a6a8aaf071b4aca74c1d85eda7
Unknown Developer <138309645+Unknown-Dev0001@users.noreply.github.com>
2025-06-13 14:20:24 +0530

    Update logo.jsx

--- components/page/logo.jsx
+++ components/page/logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
     return (
         <Link href="/" className="select-none">
             <div>
-                <h1 className="text-2xl font-bold">Wave<span className="opacity-50">Tune</span></h1>
+                <h1 className="text-2xl font-bold">Lyra<span className="opacity-50">Tune</span></h1>
             </div>
         </Link>
     )
